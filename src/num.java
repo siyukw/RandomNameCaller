4093e74d248236039e940ee1f93bc9f2590b5a8d
Siyu Kat Wang <siyukw@gmail.com>
2017-07-10 17:31:55 -0400

    version control

--- src/num.java
+++ src/num.java
@@ -1,13 +1,15 @@
 import java.util.*;
+
 public class num {
     public static void main(String[] args) {
         Random generate = new Random();
         String[] name = {"Anushka Rawat",
                 "Arya Sharma", "Fajr Eutsey", "Jada Bozeman", "Janae Motley",
-                "Kayla Hartland", "Maya Pandurangan", "Mehak Juneja", "Mona Cobb",
+                "Amanda Jones", "Maya Pandurangan", "Mehak Juneja", "Mona Cobb",
                 "Pallavi Kanoor", "Pratiti Manojkumar", "Saga Elsekhely",
                 "Sanniyah Knight", "Segiade Olajuwon", "Shivani Chandrasekaran",
                 "Simone Pipkin", "Wendy Yupa", "Sophia Augustine"};
-        System.out.println(name[generate.nextInt(18)]);
+
+        System.out.println(name[generate.nextInt(17)]);
     }
 }
