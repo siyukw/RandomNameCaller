ccd48c4399cef682f00414ffac42c19a7cc9c9b3
Siyu Kat Wang <siyukw@gmail.com>
2017-07-10 17:56:43 -0400

    made cold caller easier to use and added graphic effects

--- src/num.java
+++ src/num.java
@@ -1,15 +1,39 @@
+import java.awt.*;
 import java.util.*;
 
 public class num {
     public static void main(String[] args) {
-        Random generate = new Random();
+        /* fulllist of names
+        String[] name = {"Anushka Rawat",
+                "Arya Sharma", "Fajr Eutsey", "Jada Bozeman", "Janae Motley",
+                "Amanda Jones", "Maya Pandurangan", "Mehak Juneja", "Mona Cobb",
+                "Pallavi Kanoor", "Pratiti Manojkumar", "Saga Elsekhely",
+                "Sanniyah Knight", "Segiade Olajuwon", "Shivani Chandrasekaran",
+                "Simone Pipkin", "Wendy Yupa", "Sophia Augustine"};*/
         String[] name = {"Anushka Rawat",
                 "Arya Sharma", "Fajr Eutsey", "Jada Bozeman", "Janae Motley",
                 "Amanda Jones", "Maya Pandurangan", "Mehak Juneja", "Mona Cobb",
                 "Pallavi Kanoor", "Pratiti Manojkumar", "Saga Elsekhely",
                 "Sanniyah Knight", "Segiade Olajuwon", "Shivani Chandrasekaran",
                 "Simone Pipkin", "Wendy Yupa", "Sophia Augustine"};
-
-        System.out.println(name[generate.nextInt(17)]);
+        String[] absent = {};
+        String result = caller(name);
+        draw(result);
     }
+
+    public static String caller(String[] names) {
+        Random generate = new Random();
+        int count = names.length;
+        return names[generate.nextInt(count - 1)];
+    }
+
+    public static void draw(String name) {
+        DrawingPanel p = new DrawingPanel(1000, 600);
+        Graphics g = p.getGraphics();
+        p.setBackground(new Color(146, 208, 183));
+        Font newFont = g.getFont().deriveFont(g.getFont().getSize() * 7F);
+        g.setFont(newFont);
+        g.drawString(name, 200, 250);
+    }
+
 }
